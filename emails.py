b1930a025a483f147b32a2c1f41050d50a18c816
Ethan Steininger <esteininger21@gmail.com>
2017-02-21 11:05:27 -0500

    Build object of users & emails

--- emails.py
+++ emails.py
@@ -1,15 +1,11 @@
-import urllib2, ssl, json, re, urllib
-import requests
-try:
-    from BeautifulSoup import BeautifulSoup
-except ImportError:
-    from bs4 import BeautifulSoup
+import urllib2, ssl, json, re, sys
 
-URL = 'http://fashionista.com/2016/12/instagram-beauty-makeup-influencers-2016'
+#add usernames
+listUsers = ['iskra', 'fandemic', 'emglamguru']
 
 def getEmail(account):
-    context = ssl._create_unverified_context()
     url = 'https://www.instagram.com/' + account + '/?__a=1'
+    context = ssl._create_unverified_context()
     response = urllib2.urlopen(url, context=context)
     for line in response:
         jsonContent = json.loads(line)
@@ -20,19 +16,16 @@ def getEmail(account):
     except:
         return False
 
-def buildDictionary():
-    soup = BeautifulSoup(urllib2.urlopen(URL))
-    starsLeads = []
-    for x in soup.findAll('h3'):
-        v = {}
-        for y in x.findAll('a'):
-            y = ''.join(map(str, y.contents))
-            stars = y.split('@')
-            v['email'] = getEmail(stars[1])
-            print v['email']
-        # for z in x:
-        #     print z.string.split(',')
-        starsLeads.append(v)
-#     return starsLeads
+def buildDict():
+    dictUsers = []
+    for user in listUsers:
+        if getEmail(user):
+            u = {}
+            u['user'] = user
+            u['email'] = getEmail(user)
+            u['lead'] = False
+            u['day'] = 0
+            dictUsers.append(u)
+    return dictUsers
 
-buildDictionary()
+print buildDict()
